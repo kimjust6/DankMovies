861afbbdea7873567fbff2e121276b9f211b7c52
unknown <mletemps@hotmail.com>
2022-05-10 23:19:19 -0400

    imported new components and services into app.module and added homepagecomponent to app.component for testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from 'src/app/components/homepage/homepage.component';
+import { ApiServiceService } from './services/tmdb-api.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    ApiServiceService,
+    
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
