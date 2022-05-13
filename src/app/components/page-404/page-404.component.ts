f76334678b8f56278a2ee46a8c4c85d816979b29
unknown <mletemps@hotmail.com>
2022-05-12 22:16:44 -0400

    updated the folder and filename for the 404

--- src/app/components/page-404/page-404.component.ts
+++ src/app/components/page-404/page-404.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-page404',
-  templateUrl: './page404.component.html',
-  styleUrls: ['./page404.component.scss']
+  selector: 'app-page-404',
+  templateUrl: './page-404.component.html',
+  styleUrls: ['./page-404.component.scss']
 })
 export class Page404Component implements OnInit {
 
