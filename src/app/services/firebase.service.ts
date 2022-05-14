3e44b177c73d6167735471f8ae1e8042929d2334
unknown <mletemps@hotmail.com>
2022-05-13 23:48:17 -0400

    started firebase service

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,9 +1,64 @@
 import { Injectable } from '@angular/core';
+import {
+  Auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from '@angular/fire/auth'
+import {
+  addDoc, Firestore, collection
+} from '@angular/fire/firestore'
+
+import { UUID } from 'angular2-uuid';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseService {
+  private dbInstance: any;
+  private uuid: string | undefined;
 
-  constructor() { }
+  constructor(
+    private auth: Auth,
+  ) {
+
+  }
+
+  myAlert(alertMessage: string) {
+    alert(alertMessage);
+  }
+
+  handleRegister(value: any) {
+    createUserWithEmailAndPassword(this.auth, value.email, value.password).then((response) => {
+      console.log(response);
+    })
+      .catch((error) => {
+        this.myAlert(error);
+      });
+  }
+
+  handleLogin(value: any) {
+    signInWithEmailAndPassword(this.auth, value.email, value.password).then((response) => {
+      console.log(response);
+    })
+      .catch((error) => {
+        this.myAlert(error);
+      });
+  }
+
+  addUser() {
+
+  }
+
+  addMovie(movieID: number, date: Date) {
+
+  }
+
+  generateUUID() {
+    this.uuid = UUID.UUID();
+    return this.uuid;
+  }
+
+
 }
