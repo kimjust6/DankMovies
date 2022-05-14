e6f7e07f3adba75eba34bb9a332f938306f60a6d
unknown <mletemps@hotmail.com>
2022-05-13 22:50:00 -0400

    added login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomepageComponent } from './components/homepage/homepage.component';
 import { Page404Component } from './components/page-404/page-404.component';
 import { MovieSearchComponent } from './components/movie-search/movie-search.component';
 import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomepageComponent },
   { path: 'movie/search/:query/:pageNo', component: MovieSearchComponent },
   { path: 'movie/details/:movieID', component: MovieDetailsComponent },
+  { path: 'login', component: LoginComponent },
   { path: '**', pathMatch: 'full', component: Page404Component },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,52 +1,67 @@
+// modules
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app-routing.module';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input'
 import { MatIconModule } from '@angular/material/icon'
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
+// components
 import { AppComponent } from './app.component';
 import { HomepageComponent } from 'src/app/components/homepage/homepage.component';
+import { LoginComponent } from './components/login/login.component';
 import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
+import { MovieSearchComponent } from './components/movie-search/movie-search.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { Page404Component } from './components/page-404/page-404.component';
 
+// services
 import { tmdbAPIService } from './services/tmdb-api.service';
-import { MovieSearchComponent } from './components/movie-search/movie-search.component';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { FirebaseService } from './services/firebase.service';
+
+// firestore
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
+    LoginComponent,
     MovieDetailsComponent,
+    MovieSearchComponent,
     NavbarComponent,
     Page404Component,
-    MovieSearchComponent,
   ],
   imports: [
     AppRoutingModule,
+    BrowserAnimationsModule,
     BrowserModule,
-    MatToolbarModule,
-    MatSidenavModule,
     HttpClientModule,
     MatButtonModule,
-    BrowserAnimationsModule,
+    MatFormFieldModule,
     MatInputModule,
+    MatSidenavModule,
     MatIconModule,
+    MatToolbarModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
   ],
   providers: [
     tmdbAPIService,
+    FirebaseService,
+
+    {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'fill'}}
 
   ],
   bootstrap: [AppComponent]
