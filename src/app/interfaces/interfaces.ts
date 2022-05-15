7b5e6129a127cf865599707c6e58a621f4b8fa04
unknown <mletemps@hotmail.com>
2022-05-15 06:58:12 -0400

    updated interfaces in the simple data source and interface interfaces

--- src/app/interfaces/interfaces.ts
+++ src/app/interfaces/interfaces.ts
@@ -1,3 +1,26 @@
-export interface Interfaces {
+import { DataSource } from '@angular/cdk/collections';
+import { Observable } from 'rxjs';
+
+export interface Movie {
+    budget: number;
+    collectionID: number;
+    collectionName: string;
+    failed: string [];
+    filmTitle: string;
+    movieID: number;
+    overview: string;
+    posterPath: string;
+    rating: number;
+    releaseDate: Date;
+    revenue: number;
+    runtime: number;
+    tmdbRating: number;
+    tmdbVoteCount: number;
+    watchDate: Date;
+    watched: string[];
 }
 
+export interface SimpleDataSource<T> extends DataSource<T> {
+    connect(): Observable<T[]>;
+    disconnect(): void;
+  }
