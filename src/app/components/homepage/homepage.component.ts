03521a6e37313c5251290bf9c73f160011622ff8
unknown <mletemps@hotmail.com>
2022-05-28 10:22:09 -0400

    added table filtering and sorting

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatSort, Sort } from '@angular/material/sort';
+import { MatSort } from '@angular/material/sort';
 import { FirebaseService } from 'src/app/services/firebase.service';
 import { tmdbAPIService } from 'src/app/services/tmdb-api.service';
-
+import { MatTableDataSource } from '@angular/material/table';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
 
 
 @Component({
@@ -13,33 +15,54 @@ import { tmdbAPIService } from 'src/app/services/tmdb-api.service';
 export class HomepageComponent implements OnInit {
   // variable that is set when the api call is done loading
   public finishedLoading: boolean = false;
-  // the array that holsd the columns of the table
+  // the array that holds the columns of the table to show
   public displayedColumns: string[] = ['index', 'poster', 'movieTitle', 'runtime', 'tmdbRating', 'peoplePresent'];
+  // the MatTableDataSource that holds the movie data
+  public movieTable: any;
+  // the form for filtering films
+  public formFilter: FormGroup = this.fb.group({
+    filter: [null, []],
+  });
 
-  public movieTable: any = [];
+  @ViewChild(MatSort) sort: MatSort = new MatSort();
+  @ViewChild(MatPaginator) paginator: any = MatPaginator;
 
   constructor(
     private tmdbAPI: tmdbAPIService,
     private firebase: FirebaseService,
+    private fb: FormBuilder,
   ) {
 
   }
 
   ngOnInit(): void {
+    this.populateMovieTable();
+
+  }
+
+  // filter out the movies from the table
+  applyMovieFilter(filterString: string) {
+    this.movieTable.filter = filterString.trim().toLowerCase();
+  }
+
+  // populate the movie table
+  populateMovieTable() {
+    // initialize the movie table
+    let tempMovieTable: any = [];
     // get all the movies and store it in table
     this.firebase.getAllMovies()
       .then((res) => {
-        this.movieTable = res;
+        tempMovieTable = res;
 
         // create the index for the films so that we don't have to handle it on server side
-        for (let i = 0; i < this.movieTable.length; ++i) {
-          this.movieTable[i].index = i + 1;
+        for (let i = 0; i < tempMovieTable.length; ++i) {
+          tempMovieTable[i].index = i + 1;
         }
+
+        this.movieTable = new MatTableDataSource(tempMovieTable);
+        this.movieTable.sort = this.sort;
+        this.movieTable.paginator = this.paginator;
         this.finishedLoading = true;
-        console.log(this.movieTable);
       });
   }
-
-
-
 }
