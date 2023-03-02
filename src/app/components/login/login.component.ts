2e91ed074ce7b6d4d79adf846b0ba1094059f59c
Justin Kim <mletemps@hotmail.com>
2023-03-02 01:22:20 -0500

    added login with google button and did some small styling fixes

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,78 +1,73 @@
 import {
-  SocialAuthService,
-  FacebookLoginProvider,
-  GoogleLoginProvider,
-  SocialUser,
+	SocialAuthService,
+	FacebookLoginProvider,
+	GoogleLoginProvider,
+	SocialUser,
 } from '@abacritt/angularx-social-login';
 import { Component, OnInit } from '@angular/core';
-import { User } from '@angular/fire/auth';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FirebaseService } from 'src/app/services/firebase.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  private user: SocialUser = new SocialUser;
-  public isLoggedIn: boolean = false;
+	private user: SocialUser = new SocialUser();
+	public isLoggedIn: boolean = false;
 
-  public formLogin: FormGroup = this.fb.group({
-    email: [
-      null,
-      [
-        Validators.required,
-        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
-      ],
-    ],
-    password: [null, [Validators.required, Validators.minLength(8)]],
-  });
+	public formLogin: FormGroup = this.fb.group({
+		email: [
+			null,
+			[
+				Validators.required,
+				Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+			],
+		],
+		password: [null, [Validators.required, Validators.minLength(8)]],
+	});
 
-  public formRegister: FormGroup = this.fb.group({
-    email: [
-      null,
-      [
-        Validators.required,
-        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
-      ],
-    ],
-    firstName: [null, [Validators.required]],
-    lastName: [null, [Validators.required]],
-    password: [null, [Validators.required, Validators.minLength(8)]],
-    repeatPassword: [null, [Validators.required, Validators.minLength(8)]],
-  });
-  constructor(
-    private fb: FormBuilder,
-    private firebaseService: FirebaseService,
-    private socialAuthService: SocialAuthService
-  ) {}
+	public formRegister: FormGroup = this.fb.group({
+		email: [
+			null,
+			[
+				Validators.required,
+				Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+			],
+		],
+		firstName: [null, [Validators.required]],
+		lastName: [null, [Validators.required]],
+		password: [null, [Validators.required, Validators.minLength(8)]],
+		repeatPassword: [null, [Validators.required, Validators.minLength(8)]],
+	});
+	constructor(
+		private fb: FormBuilder,
+		private firebaseService: FirebaseService,
+		private socialAuthService: SocialAuthService
+	) {}
 
-  ngOnInit(): void {
-    this.socialAuthService.authState.subscribe((user) => {
-      this.user = user;
-      this.isLoggedIn = !user;
-    });
-  }
+	ngOnInit(): void {
+		this.socialAuthService.authState.subscribe(user => {
+			this.user = user;
+			this.isLoggedIn = !user;
+		});
+	}
 
-  handleNormalLogin() {
-    console.log(this.formLogin.value);
-    // this.firebase.addUser(this.form.value);
-  }
+	handleNormalLogin() {
+		console.log(this.formLogin.value);
+		// this.firebase.addUser(this.form.value);
+	}
 
-  signInWithFB(): void {
-    this.socialAuthService.signIn(FacebookLoginProvider.PROVIDER_ID);
-  }
+	signInWithFB(): void {
+		this.socialAuthService.signIn(FacebookLoginProvider.PROVIDER_ID);
+	}
 
-  signInWithGoogle(): void {
-    this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID);
-  }
+	handleNormalRegister() {
+		console.log(this.formRegister.value);
+	}
 
-  handleNormalRegister() {
-    console.log(this.formRegister.value);
-  }
-
-  signOut(): void {
-    this.socialAuthService.signOut();
-  }
+	signOut(): void {
+		this.socialAuthService.signOut();
+	}
 }
