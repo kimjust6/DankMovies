f5c266ef892d3a96e0328c2d5a10f7af3e529084
Justin Kim <mletemps@hotmail.com>
2023-03-01 22:40:34 -0500

    linting and added new oauth service

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -2,15 +2,10 @@ import { Injectable } from '@angular/core';
 import {
   Auth,
   createUserWithEmailAndPassword,
-  signInWithEmailAndPassword
-} from '@angular/fire/auth'
+  signInWithEmailAndPassword,
+} from '@angular/fire/auth';
 
-import {
-  addDoc,
-  Firestore,
-  collection,
-  doc
-} from '@angular/fire/firestore'
+import { addDoc, Firestore, collection, doc } from '@angular/fire/firestore';
 import { deleteDoc, getDocs, setDoc } from '@firebase/firestore';
 
 import { UUID } from 'angular2-uuid';
@@ -18,9 +13,8 @@ import { tmdbAPIService } from './tmdb-api.service';
 
 import { Movie } from '../interfaces/interfaces';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FirebaseService {
   private uuid: string | undefined;
@@ -29,13 +23,11 @@ export class FirebaseService {
   private USER_COLLECTION = 'Users';
   public NULL_VALUE = -69;
 
-
   constructor(
     private auth: Auth,
     private firestore: Firestore,
-    private tmdbAPI: tmdbAPIService,
-  ) {
-  }
+    private tmdbAPI: tmdbAPIService
+  ) {}
 
   /**
    * @description this method creates an alert message
@@ -77,7 +69,7 @@ export class FirebaseService {
 
   /**
    * @name addUserInfo
-   * @description this the method that will hold the user data in the firestore db 
+   * @description this the method that will hold the user data in the firestore db
    * (real addUser will use firebase authentication)
    * @param value the value of the form that will be entered into the firestore db
    */
@@ -85,12 +77,11 @@ export class FirebaseService {
     const dbInstance = collection(this.firestore, this.USER_COLLECTION);
     addDoc(dbInstance, _value)
       .then(() => {
-        console.log("added user");
+        console.log('added user');
       })
       .catch((error) => {
         this.myAlert(error.message);
       });
-
   }
 
   /**
@@ -100,7 +91,11 @@ export class FirebaseService {
    * @param _date the date that the film was watched
    */
   public addMovieByValue(_value: any, _date: Date): Promise<Movie> {
-    return this.addMovieByValueAndCollectionID(_value, _date, _date.getTime().toString() + this.generateUUID());
+    return this.addMovieByValueAndCollectionID(
+      _value,
+      _date,
+      _date.getTime().toString() + this.generateUUID()
+    );
   }
 
   /**
@@ -109,7 +104,11 @@ export class FirebaseService {
    * @param _value the tmdb movie data
    * @param _date the date that the film was watched
    */
-  public addMovieByValueAndCollectionID(_value: any, _date: Date, _collectionID: string): Promise<Movie> {
+  public addMovieByValueAndCollectionID(
+    _value: any,
+    _date: Date,
+    _collectionID: string
+  ): Promise<Movie> {
     return this.updateMovieByValueByCollectionID(_value, _date, _collectionID);
   }
 
@@ -118,22 +117,33 @@ export class FirebaseService {
    * @param _movieID this is the id of the movie
    * @param _date this is the date that the film was watched
    * @param _collectionID this is the unique id that the server will save as the collectionID
-   * @returns 
+   * @returns
    */
-  public async updateMovieByIDByCollectionID(_movieID: number, _date: Date, _collectionID: string): Promise<Movie> {
+  public async updateMovieByIDByCollectionID(
+    _movieID: number,
+    _date: Date,
+    _collectionID: string
+  ): Promise<Movie> {
     const movieDetail = await this.tmdbAPI.getMovieDetailsByID(_movieID);
-    return this.updateMovieByValueByCollectionID(movieDetail, _date, _collectionID);
+    return this.updateMovieByValueByCollectionID(
+      movieDetail,
+      _date,
+      _collectionID
+    );
   }
 
   /**
    * @name updateMovieByValueByCollectionID
-   * @param value 
-   * @param date 
-   * @param collectionID 
+   * @param value
+   * @param date
+   * @param collectionID
    * @returns Promise<Movie>
    */
-  public async updateMovieByValueByCollectionID(value: any, date: Date, collectionID: string): Promise<Movie> {
-
+  public async updateMovieByValueByCollectionID(
+    value: any,
+    date: Date,
+    collectionID: string
+  ): Promise<Movie> {
     // initialize movie
     let movie = {
       budget: value?.budget,
@@ -143,7 +153,7 @@ export class FirebaseService {
       filmTitle: value?.title,
       movieID: value?.id,
       overview: value?.overview,
-      posterPath: (this.tmdbAPI.getImageBaseURL() + value.poster_path),
+      posterPath: this.tmdbAPI.getImageBaseURL() + value.poster_path,
       rating: -1,
       releaseDate: value?.release_date,
       revenue: value?.revenue,
@@ -153,25 +163,34 @@ export class FirebaseService {
       watchDate: date,
       failed: [],
       watched: [],
-    }
+    };
 
     // check for invalid data
-    movie.budget ? true : movie.budget = this.NULL_VALUE;
-    movie.movieCollectionID ? true : movie.movieCollectionID = this.NULL_VALUE;
-    movie.collectionName ? true : movie.collectionName = this.NULL_VALUE;
-    movie.filmTitle ? true : movie.filmTitle = this.NULL_VALUE;
-    movie.movieID ? true : movie.movieID = this.NULL_VALUE;
-    movie.overview ? true : movie.overview = this.NULL_VALUE;
-    movie.posterPath ? true : movie.posterPath = '';
-    movie.posterPath === '' ? movie.posterPath = (this.tmdbAPI.getImageBaseURL() + value.backdrop_path) : false;
-    movie.rating ? true : movie.rating = this.NULL_VALUE;
-    movie.releaseDate ? true : movie.releaseDate = this.NULL_VALUE;
-    movie.revenue ? true : movie.revenue = this.NULL_VALUE;
-    movie.runtime ? true : movie.runtime = this.NULL_VALUE;
-    movie.tmdbRating ? true : movie.tmdbRating = this.NULL_VALUE;
-    movie.tmdbVoteCount ? true : movie.tmdbVoteCount = this.NULL_VALUE;
+    movie.budget ? true : (movie.budget = this.NULL_VALUE);
+    movie.movieCollectionID
+      ? true
+      : (movie.movieCollectionID = this.NULL_VALUE);
+    movie.collectionName ? true : (movie.collectionName = this.NULL_VALUE);
+    movie.filmTitle ? true : (movie.filmTitle = this.NULL_VALUE);
+    movie.movieID ? true : (movie.movieID = this.NULL_VALUE);
+    movie.overview ? true : (movie.overview = this.NULL_VALUE);
+    movie.posterPath ? true : (movie.posterPath = '');
+    movie.posterPath === ''
+      ? (movie.posterPath =
+          this.tmdbAPI.getImageBaseURL() + value.backdrop_path)
+      : false;
+    movie.rating ? true : (movie.rating = this.NULL_VALUE);
+    movie.releaseDate ? true : (movie.releaseDate = this.NULL_VALUE);
+    movie.revenue ? true : (movie.revenue = this.NULL_VALUE);
+    movie.runtime ? true : (movie.runtime = this.NULL_VALUE);
+    movie.tmdbRating ? true : (movie.tmdbRating = this.NULL_VALUE);
+    movie.tmdbVoteCount ? true : (movie.tmdbVoteCount = this.NULL_VALUE);
 
-    const dataToUpdate = doc(this.firestore, this.MOVIE_COLLECTION, collectionID);
+    const dataToUpdate = doc(
+      this.firestore,
+      this.MOVIE_COLLECTION,
+      collectionID
+    );
     setDoc(dataToUpdate, movie);
 
     return movie;
@@ -181,12 +200,11 @@ export class FirebaseService {
 
     //add the movie to the db (firebase auto generates ids)
     // const querySnapshot = await addDoc(dbInstace, movie);
-
   }
 
   /**
    * @name addMovieByID
-   * @description this method will look up the details of the movie using the tmdb api and saved it 
+   * @description this method will look up the details of the movie using the tmdb api and saved it
    * into the firestore db
    * @param _movieID this is the id of the movie that will be returned
    * @param _date this is the date that the movie was watched
@@ -198,15 +216,23 @@ export class FirebaseService {
 
   /**
    * @name addMovieByIDandCollectionID
-   * @description this method will look up the details of the movie using the tmdb api and saved it 
+   * @description this method will look up the details of the movie using the tmdb api and saved it
    * into the firestore db
    * @param _movieID this is the id of the movie that will be returned
    * @param _date this is the date that the movie was watched
    * @param _collectionID this is the collection id that will be manually set
    */
-  public async addMovieByIDandCollectionID(_movieID: number, _date: Date, _collectionID: string): Promise<Movie> {
+  public async addMovieByIDandCollectionID(
+    _movieID: number,
+    _date: Date,
+    _collectionID: string
+  ): Promise<Movie> {
     const movieDetail = await this.tmdbAPI.getMovieDetailsByID(_movieID);
-    return this.addMovieByValueAndCollectionID(movieDetail, _date, _collectionID);
+    return this.addMovieByValueAndCollectionID(
+      movieDetail,
+      _date,
+      _collectionID
+    );
   }
 
   /**
@@ -220,7 +246,7 @@ export class FirebaseService {
     let i = 0;
     querySnapshot.forEach((doc) => {
       // doc.data() is never undefined for query doc snapshots
-      this.movieArray[i++] = (doc.id, " => ", doc.data());
+      this.movieArray[i++] = (doc.id, ' => ', doc.data());
     });
     // convert firebase timestamp to date
     for (let movie of this.movieArray) {
@@ -235,11 +261,14 @@ export class FirebaseService {
    * @param collectionID this is the id of the document that is to be deleted
    */
   public async deleteMovieByCollectionID(collectionID: string): Promise<void> {
-    const dataToDelete = doc(this.firestore, this.MOVIE_COLLECTION, collectionID);
+    const dataToDelete = doc(
+      this.firestore,
+      this.MOVIE_COLLECTION,
+      collectionID
+    );
     try {
       await deleteDoc(dataToDelete);
-    }
-    catch (error: any) {
+    } catch (error: any) {
       this.myAlert(error.message);
     }
   }
@@ -253,6 +282,4 @@ export class FirebaseService {
     this.uuid = UUID.UUID();
     return this.uuid;
   }
-
-
 }
