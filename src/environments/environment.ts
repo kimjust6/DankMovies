193c4b9e7f92f07f0610a43e7ddad34dd2c1d0e1
Justin Kim <mletemps@hotmail.com>
2023-03-05 19:40:21 -0500

    updated environment variables formatting

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,22 +4,22 @@
 
 export const environment = {
 	FIREBASE: {
-		projectId: process.env.FIRE_PROJECT_ID || "projectId",
-		appId: process.env.FIRE_APP_ID || "",
-		storageBucket: process.env.FIRE_STORAGE_BUCKET || "",
-		locationId: process.env.FIRE_LOCATION_ID || "",
-		apiKey: process.env.FIRE_API_KEY || "",
-		authDomain: process.env.FIRE_AUTH_DOMAIN || "",
-		messagingSenderId: process.env.FIRE_MESSAGING_SENDER_ID || "",
-		measurementId: process.env.FIRE_MEASUREMENT_ID || "",
+		projectId: process.env['FIRE_PROJECT_ID'] || 'projectId',
+		appId: process.env['FIRE_APP_ID'] || '',
+		storageBucket: process.env['FIRE_STORAGE_BUCKET'] || '',
+		locationId: process.env['FIRE_LOCATION_ID'] || '',
+		apiKey: process.env['FIRE_API_KEY'] || '',
+		authDomain: process.env['FIRE_AUTH_DOMAIN'] || '',
+		messagingSenderId: process.env['FIRE_MESSAGING_SENDER_ID'] || '',
+		measurementId: process.env['FIRE_MEASUREMENT_ID'] || '',
 	},
 	GOOGLE: {
-		CLIENT_ID: process.env.GOOGLE_CLIENT_ID || '',
-		CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET || '',
+		CLIENT_ID: process.env['GOOGLE_CLIENT_ID'] || '',
+		CLIENT_SECRET: process.env['GOOGLE_CLIENT_SECRET'] || '',
 	},
 	TMDB: {
-		API_KEY_V3: process.env.TMDB_API_KEY_V3 || '',
-		API_KEY_V4: process.env.TMDB_API_KEY_V4 || '',
+		API_KEY_V3: process.env['TMDB_API_KEY_V3'] || 'TMDB_API_KEY_V3',
+		API_KEY_V4: process.env['TMDB_API_KEY_V4'] || '',
 	},
 	production: false,
 };
