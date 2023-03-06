713667ca9c2915c3aae546bea16048743ab93b32
Justin Kim <mletemps@hotmail.com>
2023-03-05 22:41:42 -0500

    removed line for debugging purposes

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -42,8 +42,6 @@ export class HomepageComponent implements OnInit {
     private dialog: MatDialog,
     private router: Router,
   ) {
-    this.nice = "projectId: " + environment.FIREBASE.projectId + ", CLIENT_ID: " + environment.GOOGLE.CLIENT_ID + ", API_KEY_V3: "+ environment.TMDB.API_KEY_V3 + " "+ process.env["TMDB_API_KEY_V3"]
-    
   }
 
   ngOnInit(): void {
